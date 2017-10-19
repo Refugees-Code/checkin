68d132f15529602e1cd8a9bfff0688bf656217fc
Dario Seidl <darioseidl@fastmail.fm>
2017-10-19 10:45:15 +0200

    add auth enabled setting

--- src/main/java/at/refugeescode/checkin/config/SecurityConfig.java
+++ src/main/java/at/refugeescode/checkin/config/SecurityConfig.java
@@ -1,9 +1,11 @@
 package at.refugeescode.checkin.config;
 
 import com.google.common.collect.ImmutableList;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -15,32 +17,41 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    public static final String CLIENT_ROLE = "CLIENT";
+    public static final String ADMIN_ROLE = "ADMIN";
 
+    @Value("${checkin.auth.enabled}")
+    private boolean authEnabled;
     @Value("${checkin.auth.username}")
-    private String username;
+    private String authUsername;
     @Value("${checkin.auth.password}")
-    private String password;
+    private String authPassword;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
-                .cors()
-                .and()
-//                .httpBasic()
-//                .and()
-//                .authorizeRequests()
-//                .anyRequest().hasRole(CLIENT_ROLE)
-//                .and()
-                .csrf().disable();
+        if (authEnabled)
+            http
+                    .cors()
+                    .and()
+                    .httpBasic()
+                    .and()
+                    .authorizeRequests()
+                    .anyRequest().hasRole(ADMIN_ROLE)
+                    .and()
+                    .csrf().disable();
+        else
+            http
+                    .cors()
+                    .and()
+                    .csrf().disable();
     }
 
-//    @Autowired
-//    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-//        auth
-//                .inMemoryAuthentication()
-//                .withUser(username).password(password).roles(CLIENT_ROLE);
-//    }
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        if (authEnabled)
+            auth
+                    .inMemoryAuthentication()
+                    .withUser(authUsername).password(authPassword).roles(ADMIN_ROLE);
+    }
 
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {

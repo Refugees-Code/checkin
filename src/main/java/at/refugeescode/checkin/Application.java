af267eb6478621909e81d47c13f1a17836f23f5c
Dario Seidl <darioseidl@fastmail.fm>
2017-11-03 15:55:23 +0100

    add tracing

--- src/main/java/at/refugeescode/checkin/Application.java
+++ src/main/java/at/refugeescode/checkin/Application.java
@@ -4,12 +4,14 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EntityScan(basePackageClasses = {Application.class, Jsr310JpaConverters.class})
 @SpringBootApplication
 @EnableScheduling
+@EnableAspectJAutoProxy
 @Slf4j
 public class Application {
 

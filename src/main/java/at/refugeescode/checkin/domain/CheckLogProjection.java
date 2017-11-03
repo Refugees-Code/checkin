fd35c4193126d01c16c01939899d1a12bde95a4b
Dario Seidl <darioseidl@fastmail.fm>
2017-11-03 21:10:54 +0100

    add check log projection with name

---
+++ src/main/java/at/refugeescode/checkin/domain/CheckLogProjection.java
@@ -0,0 +1,19 @@
+package at.refugeescode.checkin.domain;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.rest.core.config.Projection;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+
+@Projection(name = "log", types = Checkin.class)
+public interface CheckLogProjection {
+
+    LocalDateTime getTime();
+
+    Duration getDuration();
+
+    @Value("#{target.person.name}")
+    String getName();
+
+}

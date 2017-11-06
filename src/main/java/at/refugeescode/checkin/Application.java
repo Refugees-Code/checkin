77e3affef698739f40925a07ebc159d6998eef14
Dario Seidl <darioseidl@fastmail.fm>
2017-11-06 13:44:08 +0100

    add TaskScheduler bean

--- src/main/java/at/refugeescode/checkin/Application.java
+++ src/main/java/at/refugeescode/checkin/Application.java
@@ -4,9 +4,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
+import org.springframework.scheduling.TaskScheduler;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.concurrent.ConcurrentTaskScheduler;
 
 @EntityScan(basePackageClasses = {Application.class, Jsr310JpaConverters.class})
 @SpringBootApplication
@@ -14,6 +17,11 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableAspectJAutoProxy
 @Slf4j
 public class Application {
+
+    @Bean
+    public TaskScheduler taskScheduler() {
+        return new ConcurrentTaskScheduler();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);

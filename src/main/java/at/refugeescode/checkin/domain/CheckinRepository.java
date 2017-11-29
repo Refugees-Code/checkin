a05fb222f053dbd2093d75a700fdc9b59b3bc5ed
Dario Seidl <darioseidl@fastmail.fm>
2017-11-29 12:16:07 +0100

    Checkin findByOrderByTimeDesc endpoint

--- src/main/java/at/refugeescode/checkin/domain/CheckinRepository.java
+++ src/main/java/at/refugeescode/checkin/domain/CheckinRepository.java
@@ -1,5 +1,7 @@
 package at.refugeescode.checkin.domain;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
@@ -7,6 +9,8 @@ import java.util.List;
 
 @RepositoryRestResource(collectionResourceRel = "checkins", path = "checkins")
 public interface CheckinRepository extends JpaRepository<Checkin, Long> {
+
+    Page<Checkin> findByOrderByTimeDesc(Pageable pageable);
 
     List<Checkin> findByPersonOrderByTime(Person person);
 
